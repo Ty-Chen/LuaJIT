9e4a5050f47ee74d32fb3bfcd272b1279d11a43f
Ty-Chen <369494003@qq.com>
2021-08-27 21:36:24 +0800

    Add a little example by vs2019 sln

---
+++ Performance/Performance/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "lua.hpp"
+int main()
+{
+    lua_State *L = luaL_newstate(); /*创建一个解释器句柄*/
+    luaL_openlibs(L); /*打开所有的Lua库*/
+    luaL_loadfile(L, "../LuaJIT-Debug/t1.lua"); /*调入Lua脚本文件，注意路径*/
+    lua_pcall(L, 0, 0, 0); /*执行Lua脚本*/
+    lua_close(L); /*关闭句柄*/
+    return 0;
+}
