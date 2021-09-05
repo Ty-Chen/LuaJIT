e9d7c94515c3ed056effd00e4c2e273e1cbb13c4
Ty-Chen <369494003@qq.com>
2021-09-05 21:11:21 +0800

    Update t4.lua

--- src/t4.lua
+++ src/t4.lua
@@ -3,7 +3,6 @@ local _M = {}
 local N = 1e7
 
 local function heavy()
-    print("get into heavy")
     local sum = 0
     for i = 1, N do
         sum = sum + i
@@ -22,7 +21,6 @@ local function bar()
 end
 
 function _M.main()
-    io.write("get into main")
     foo()
     bar()
 end
